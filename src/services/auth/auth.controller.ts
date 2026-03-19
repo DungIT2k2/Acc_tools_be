ca16b3f8b39bdcbf1f18f5e604a0862df4983fe6
Nguyễn Văn Tiến Dũng <116363146+DungIT2k2@users.noreply.github.com>
2026-03-19 22:48:19 +0700

    feat: add HTTP status code to login endpoint

--- src/services/auth/auth.controller.ts
+++ src/services/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,6 +6,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(200)
   login(@Body() body): object {
     return this.authService.login(body);
   }
